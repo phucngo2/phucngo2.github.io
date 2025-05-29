9075d47515e4881e3e1da5ac6f23babd76c3800e
phucngo2 <85348631+phucngo2@users.noreply.github.com>
2025-05-29 20:48:50 +0700

    config: HeroUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { HeroUIProvider } from "@heroui/react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HeroUIProvider>
+      <main className="dark text-foreground bg-background">
+        <App />
+      </main>
+    </HeroUIProvider>
+  </StrictMode>
+);
