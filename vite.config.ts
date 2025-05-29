42d43857686c871edb3c08485d5ee55dc3e0b789
phucngo2 <85348631+phucngo2@users.noreply.github.com>
2025-05-29 20:29:07 +0700

    config: tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -13,5 +14,6 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    tailwindcss(),
   ],
 });
