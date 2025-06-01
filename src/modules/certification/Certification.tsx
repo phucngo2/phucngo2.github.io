3edb2808a9ebed7ab5cd382babf5c22847082905
phucngo2 <85348631+phucngo2@users.noreply.github.com>
2025-06-01 21:02:13 +0700

    fix: mobile responsive

--- src/modules/certification/Certification.tsx
+++ src/modules/certification/Certification.tsx
@@ -6,7 +6,7 @@ import { CertificationItem } from "./CertificationItem";
 export const Certification = () => {
   return (
     <Section id={Sections.Certification} title="Certification">
-      <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {certificationData.map((item) => (
           <CertificationItem key={item.name} item={item} />
         ))}
